c4c89141871f935e3832f709d75d1e93959d9fdb
MakrosTeclas <60923412+MakrosTeclas@users.noreply.github.com>
2020-02-13 11:06:19 +0100

    Cambios para provocar fallos en PMD

--- server/src/main/java/com/example/Greeter.java
+++ server/src/main/java/com/example/Greeter.java
@@ -17,11 +17,16 @@ public class Greeter {
    * Metodo de greet modificado.
    */
   public String greet(String someone) {
-    //Long lalala = new Long("2333");
-    if(magico > 1){
-        return String.format("Hello, %s!", someone);
-    }
-     else{
+    Long lalala = new Long("2333");
+    if (magico > 1) {
+      if (true) {
+        if (true) {
+          if (true) {
+            return String.format("Hello, %s!", someone);
+          }
+        }
+      }
+    } else {
         return String.format("Hello, %s!", someone);
     }
   }
