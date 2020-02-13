1d3785bdc40db61c121dc83dab7143dcc396ce71
MakrosTeclas <60923412+MakrosTeclas@users.noreply.github.com>
2020-02-13 12:26:46 +0100

    Update Greeter.java

--- server/src/main/java/com/example/Greeter.java
+++ server/src/main/java/com/example/Greeter.java
@@ -19,7 +19,7 @@ public class Greeter {
   public String greet(String someone) {
     //Long lalala = new Long("2333");
     if (magico > 1) {
-        return String.format("Hello, %s!", someone.toUpperCase());
+        return String.format("Hello, %s!", someone);
     } else {
         return String.format("Hello, %s!", someone);
     }
